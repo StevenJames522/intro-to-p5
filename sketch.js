e28bd4895724b26c363b57c99780749e8668e345
Steven James <sjames@wlapcs.org>
2017-03-16 13:52:57 +0000

    Finished product

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup(){
+    createCanvas(600,400);
+    background(100,100,100);
+    
+}
+
+function draw(){
+    line(35, 80, 400, 200);   
+    
+    stroke(255,255,255);
+    fill(255,0,0);
+    rect(290,100,250,250);
+
+
+    stroke(0,0,0);
+    strokeWeight(4);
+    fill(0,0,255);    
+    ellipse(50,250,80,80);
+
+    
+    ellipse(525,233,50,50);
+    line(200,50,200,500);
+    line(300,300,575,300);
+    line(45,95,300,200);
+    rect(23,55,100,50);   
+}
